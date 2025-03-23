48c98972ad29780aa9a4b21ad8d065387f0fe214
ihSakib <ihsakib.dev@gmail.com>
2025-03-23 23:22:26 +0600

    Add CrossOrigin annotation to NoticeController
    Enable CORS for specific origins to allow frontend access. This includes localhost for development and the deployed app URL for production use.

--- src/main/java/org/example/restapi/controller/NoticeController.java
+++ src/main/java/org/example/restapi/controller/NoticeController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "https://notice-app.up.railway.app"})
 public class NoticeController {
 
     @Autowired
