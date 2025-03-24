adc65ae338720231b46ff74b2ae74e6c50d3d32a
ihSakib <ihsakib.dev@gmail.com>
2025-03-24 20:04:42 +0600

    Refactor notice retrieval to use repository method.

--- src/main/java/org/example/restapi/repository/NoticeRepository.java
+++ src/main/java/org/example/restapi/repository/NoticeRepository.java
@@ -4,9 +4,14 @@ import org.example.restapi.model.Notice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Integer> {
+
+    default List<Notice> findAllByOrderByDateDesc() {
+        return findAll(Sort.by(Sort.Order.desc("date")));
+    }
 
 }
--- src/main/java/org/example/restapi/service/NoticeService.java
+++ src/main/java/org/example/restapi/service/NoticeService.java
@@ -3,7 +3,6 @@ package org.example.restapi.service;
 import org.example.restapi.model.Notice;
 import org.example.restapi.repository.NoticeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -17,11 +16,7 @@ public class NoticeService {
     private NoticeRepository noticeRepository;
 
     public List<Notice> getNotices() {
-        return noticeRepository.findAll(sortByDateDesc());
-    }
-
-    private Sort sortByDateDesc() {
-        return Sort.by(Sort.Order.desc("date"));
+        return noticeRepository.findAllByOrderByDateDesc();
     }
 
     public Object getNoticeById(int id) {
